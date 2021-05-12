8e62bb28588284ed93c7876430c442d91be8f71d
Oniel <51058739+0niel@users.noreply.github.com>
2021-05-12 20:49:39 +0300

    Add tests day of week button

--- lib/screens/schedule/components/body.dart
+++ lib/screens/schedule/components/body.dart
@@ -1,5 +1,74 @@
 import 'package:flutter/material.dart';
-import 'schedule_container.dart';
+import 'package:rtu_mirea_app/screens/schedule/components/schedule_container.dart';
+
+import '../../../constants.dart';
+
+class SchedulePageView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final PageController controller = PageController(initialPage: 0);
+    return PageView(
+      /// [PageView.scrollDirection] defaults to [Axis.horizontal].
+      /// Use [Axis.vertical] to scroll vertically.
+      scrollDirection: Axis.horizontal,
+      controller: controller,
+      children: <Widget>[
+        Center(
+          child: ScheduleContainer(),
+        ),
+        Center(
+          child: ScheduleContainer(),
+        ),
+        Center(
+          child: ScheduleContainer(),
+        )
+      ],
+    );
+  }
+}
+
+class DaySelectorTabBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          margin: EdgeInsets.symmetric(
+              horizontal: kDefaultPadding, vertical: kDefaultPadding / 2),
+          child: RichText(
+            textAlign: TextAlign.center,
+            text: TextSpan(
+              children: [
+                TextSpan(
+                  text: "ПН\n",
+                  style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    color: Color(0xFFBCC1CD),
+                    fontSize: 18,
+                  ),
+                ),
+                TextSpan(
+                  text: "21",
+                  style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 19,
+                      fontWeight: FontWeight.bold),
+                ),
+              ],
+            ),
+          ),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(40),
+              topRight: Radius.circular(40),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
 
 class Body extends StatelessWidget {
   @override
@@ -19,7 +88,8 @@ class Body extends StatelessWidget {
                     ),
                   ),
                 ),
-                ScheduleContainer(),
+                DaySelectorTabBar(),
+                SchedulePageView(),
               ],
             ),
           ),
--- lib/screens/schedule/components/schedule_container.dart
+++ lib/screens/schedule/components/schedule_container.dart
@@ -4,6 +4,14 @@ import 'package:rtu_mirea_app/constants.dart';
 import 'lessons.dart';
 
 class ScheduleContainer extends StatelessWidget {
+  List<Widget> _getScheduleList() {
+    List<Widget> schedule = [];
+    for (int i = 0; i < 5; i++) {
+      schedule.add(LessonsWidget());
+    }
+    return schedule;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -25,10 +33,7 @@ class ScheduleContainer extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 30),
-              Lessons(),
-              Lessons(),
-              Lessons(),
-              Lessons(),
+              ..._getScheduleList(),
             ],
           ),
         ),
