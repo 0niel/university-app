eb8514ef666019bff4bb0af36e7d480a445be796
Oniel <51058739+0niel@users.noreply.github.com>
2021-05-19 21:05:51 +0300

    Add 'type' field to lesson model

--- lib/models/lesson.dart
+++ lib/models/lesson.dart
@@ -1,6 +1,12 @@
 class Lesson {
   final String timeStart, timeEnd;
-  final String name, teacher, room;
+  final String name, teacher, room, type;
 
-  Lesson({this.timeStart, this.timeEnd, this.name, this.teacher, this.room});
+  Lesson(
+      {this.timeStart,
+      this.timeEnd,
+      this.name,
+      this.teacher,
+      this.room,
+      this.type});
 }
