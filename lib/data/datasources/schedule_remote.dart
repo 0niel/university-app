0289f4818faa32bfb849f8e554cc56d94ad10c68
Oniel <51058739+0niel@users.noreply.github.com>
2021-07-09 22:41:25 +0300

    Add ScheduleRemoteData implementation

--- lib/data/datasources/schedule_remote.dart
+++ lib/data/datasources/schedule_remote.dart
@@ -1,1 +1,28 @@
-abstract class ScheduleRemoteData {}
+import 'package:dio/dio.dart';
+import 'package:rtu_mirea_app/data/models/schedule_model.dart';
+
+abstract class ScheduleRemoteData {
+  Future<ScheduleModel> getScheduleByGroup(String group);
+  Future<List<String>> getGroups();
+}
+
+class ScheduleRemoteDataApi implements ScheduleRemoteData {
+  static const _API_BASE_URL = 'http://127.0.0.1:5000/api/';
+
+  final Dio _dio = Dio(
+    BaseOptions(baseUrl: _API_BASE_URL),
+  );
+
+  @override
+  Future<List<String>> getGroups() async {
+    final response = await _dio.get('/schedule/groups');
+    List<String> groups = response.data['groups'];
+    return groups;
+  }
+
+  @override
+  Future<ScheduleModel> getScheduleByGroup(String group) async {
+    final response = await _dio.get('/schedule/$group/full_schedule');
+    return ScheduleModel.fromJson(response.data);
+  }
+}
