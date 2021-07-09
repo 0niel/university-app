1fd44d3376c03c5763c3fca08ec5c5b7a4771e18
Oniel <51058739+0niel@users.noreply.github.com>
2021-07-09 23:11:26 +0300

    Create schedule_repository_impl.dart

---
+++ lib/data/repositories/schedule_repository_impl.dart
@@ -0,0 +1,22 @@
+import 'package:rtu_mirea_app/domain/entities/schedule.dart';
+import 'package:rtu_mirea_app/domain/repositories/schedule_repository.dart';
+
+class ScheduleRepositoryImpl implements ScheduleRepository {
+  @override
+  Future<List<String>> getAllGroups() {
+    // TODO: implement getAllGroups
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<Schedule> getSchedule(String group) {
+    // TODO: implement getSchedule
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<bool> isGroupExist() {
+    // TODO: implement isGroupExist
+    throw UnimplementedError();
+  }
+}
