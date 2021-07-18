cdc6b5605da4cf1f292bf1beb7555b97bc12c57c
Oniel <51058739+0niel@users.noreply.github.com>
2021-07-18 21:21:56 +0300

    DOCS: update ScheduleLocalData documentation

--- lib/data/datasources/schedule_local.dart
+++ lib/data/datasources/schedule_local.dart
@@ -1,10 +1,31 @@
 import 'package:rtu_mirea_app/data/models/schedule_model.dart';
 
 abstract class ScheduleLocalData {
-  Future<List<ScheduleModel>> getScheduleFromCache(String group);
+  /// Gets the cached [List<ScheduleModel>] all saved schedules
+  /// which were added using [addScheduleToCache].
+  ///
+  /// Throws [CacheException] if no cached data is present.
+  Future<List<ScheduleModel>> getScheduleFromCache();
+
+  /// Gets the cached [List<String>] names of all available groups for
+  /// which the server has a schedule.
+  ///
+  /// Throws [CacheException] if no cached data is present.
   Future<List<String>> getGroupsFromCache();
 
+  /// Adds a [ScheduleModel] to the list of the all cached schedule.
+  /// If the schedule with this [schedule.group] is already in the cache,
+  /// it will be overwritten
   Future<void> addScheduleToCache(ScheduleModel schedule);
+
+  /// Delete the schedule from cache by [group] name.
+  ///
+  /// Throws [CacheException] if the schedule with this [group] name
+  /// is not in the cache.
+  Future<void> deleteScheduleFromCache(String group);
+
+  /// Adds a [List<String>] of [groups] to the cache. If the groups list
+  /// is already in the cache, it will be overwritten by [groups] data.
   Future<void> addGroupsToCache(List<String> groups);
 }
 
@@ -32,8 +53,14 @@ class ScheduleLocalDataImpl implements ScheduleLocalData {
   }
 
   @override
-  Future<List<ScheduleModel>> getScheduleFromCache(String group) async {
+  Future<List<ScheduleModel>> getScheduleFromCache() async {
     // TODO: implement getScheduleFromCache
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<void> deleteScheduleFromCache(String group) {
+    // TODO: implement deleteScheduleFromCache
     throw UnimplementedError();
   }
 }
