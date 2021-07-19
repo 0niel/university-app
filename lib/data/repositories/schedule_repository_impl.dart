267b7f7b9b515d37f1405e885829d06d122a9a01
Oniel <51058739+0niel@users.noreply.github.com>
2021-07-20 00:01:31 +0300

    FEATURE: create getAllGroups and getSchedule impl

--- lib/data/repositories/schedule_repository_impl.dart
+++ lib/data/repositories/schedule_repository_impl.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:internet_connection_checker/internet_connection_checker.dart';
+import 'package:rtu_mirea_app/common/errors/exceptions.dart';
 import 'package:rtu_mirea_app/common/errors/failures.dart';
 import 'package:rtu_mirea_app/data/datasources/schedule_local.dart';
 import 'package:rtu_mirea_app/data/datasources/schedule_remote.dart';
@@ -11,21 +12,50 @@ class ScheduleRepositoryImpl implements ScheduleRepository {
   final ScheduleLocalData localDataSource;
   final InternetConnectionChecker connectionChecker;
 
-  ScheduleRepositoryImpl(
-      {required this.remoteDataSource,
-      required this.localDataSource,
-      required this.connectionChecker});
+  ScheduleRepositoryImpl({
+    required this.remoteDataSource,
+    required this.localDataSource,
+    required this.connectionChecker,
+  });
 
   @override
-  Future<Either<Failure, List<String>>> getAllGroups() {
-    // TODO: implement getAllGroups
-    throw UnimplementedError();
+  Future<Either<Failure, List<String>>> getAllGroups() async {
+    if (await connectionChecker.hasConnection) {
+      try {
+        final groupsList = await remoteDataSource.getGroups();
+        localDataSource.addGroupsToCache(groupsList);
+        return Right(groupsList);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      try {
+        final localGroupsList = await localDataSource.getGroupsFromCache();
+        return Right(localGroupsList);
+      } on CacheException {
+        return Left(CacheFailure());
+      }
+    }
   }
 
   @override
-  Future<Either<Failure, Schedule>> getSchedule(String group) {
-    // TODO: implement getSchedule
-    throw UnimplementedError();
+  Future<Either<Failure, Schedule>> getSchedule(String group) async {
+    if (await connectionChecker.hasConnection) {
+      try {
+        final schedule = await remoteDataSource.getScheduleByGroup(group);
+        localDataSource.addScheduleToCache(schedule);
+        return Right(schedule);
+      } on ServerException {
+        return Left(ServerFailure());
+      }
+    } else {
+      try {
+        final localSchedule = await localDataSource.getScheduleFromCache(group);
+        return Right(localSchedule);
+      } on CacheException {
+        return Left(CacheFailure());
+      }
+    }
   }
 
   @override
