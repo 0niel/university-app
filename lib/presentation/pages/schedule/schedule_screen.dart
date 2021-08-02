0a25e929503cccf92e3e8286e93c198a72bd5bee
Oniel <51058739+0niel@users.noreply.github.com>
2021-08-02 23:19:17 +0300

    UX: remove schedule right drawer

--- lib/presentation/pages/schedule/schedule_screen.dart
+++ lib/presentation/pages/schedule/schedule_screen.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:rtu_mirea_app/presentation/theme.dart';
-import '../../colors.dart';
 import 'widgets/schedule_page_view.dart';
 
 class ScheduleScreen extends StatelessWidget {
@@ -14,27 +13,7 @@ class ScheduleScreen extends StatelessWidget {
         automaticallyImplyLeading: false,
         title: Text(
           'Расписание',
-        ),
-        actions: <Widget>[
-          Builder(
-            builder: (BuildContext context) {
-              return IconButton(
-                icon: SvgPicture.asset('assets/icons/menu.svg'),
-                onPressed: () {
-                  Scaffold.of(context).openEndDrawer();
-                },
-              );
-            },
-          ),
-        ],
-      ),
-      endDrawer: Drawer(
-        child: SafeArea(
-          child: Container(
-            child: Column(
-              children: [],
-            ),
-          ),
+          style: DarkTextTheme.title,
         ),
       ),
       body: SafeArea(
