566ca784c3f74bb6b99c9c6ed7772e15a904237b
Oniel <51058739+0niel@users.noreply.github.com>
2021-08-17 01:48:41 +0300

    REFACTOR: change slider drawer to inner_drawer

--- lib/presentation/pages/schedule/schedule_screen.dart
+++ lib/presentation/pages/schedule/schedule_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:flutter_slider_drawer/flutter_slider_drawer.dart';
+import 'package:flutter_inner_drawer/inner_drawer.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:rtu_mirea_app/domain/entities/schedule.dart';
 import 'package:rtu_mirea_app/presentation/bloc/schedule_bloc/schedule_bloc.dart';
@@ -21,19 +21,25 @@ class ScheduleScreen extends StatefulWidget {
 class _ScheduleScreenState extends State<ScheduleScreen> {
   bool _modalShown = false;
 
+  //  Current State of InnerDrawerState
+  final GlobalKey<InnerDrawerState> _innerDrawerKey =
+      GlobalKey<InnerDrawerState>();
+
   void initState() {
     super.initState();
   }
 
-  // ValueNotifier<bool> _switchValueNotifier = ValueNotifier(false);
+  ValueNotifier<bool> _switchValueNotifier = ValueNotifier(false);
 
   Widget _buildGroupButton(String group, String activeGroup, bool isActive,
       [Schedule? schedule]) {
     if (isActive) {
       return Padding(
-        padding: EdgeInsets.only(bottom: 10, right: 10),
+        padding: EdgeInsets.only(bottom: 10),
         child: Container(
-          padding: EdgeInsets.only(left: 10, right: 8),
+          padding: EdgeInsets.only(
+            left: 10,
+          ),
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -65,9 +71,9 @@ class _ScheduleScreenState extends State<ScheduleScreen> {
       );
     } else {
       return Padding(
-        padding: EdgeInsets.only(bottom: 10, right: 8),
+        padding: EdgeInsets.only(bottom: 10),
         child: Container(
-          padding: EdgeInsets.only(left: 10, right: 8),
+          padding: EdgeInsets.only(left: 10),
           child: Row(
             crossAxisAlignment: CrossAxisAlignment.center,
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -115,174 +121,182 @@ class _ScheduleScreenState extends State<ScheduleScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: SliderMenuContainer(
-        appBarColor: DarkThemeColors.background01,
-        isDraggable: false,
-        drawerIconColor: DarkThemeColors.white,
-        appBarHeight: 60,
-        slideDirection: SlideDirection.RIGHT_TO_LEFT,
-        appBarPadding: const EdgeInsets.all(0),
-        isTitleCenter: false,
-        sliderMenuOpenSize: MediaQuery.of(context).size.width * 0.80,
-        title: Text(
-          'Расписание',
-          style: DarkTextTheme.title,
-        ),
-        sliderMenu: Column(
-          children: [
-            Padding(
-              padding: EdgeInsets.symmetric(horizontal: 8, vertical: 18),
-              child: Text(
-                'Управление расписанием и группами',
-                style: DarkTextTheme.h6,
+    return InnerDrawer(
+      key: _innerDrawerKey,
+      offset: const IDOffset.horizontal(0.6),
+      swipeChild: true,
+      rightChild: SafeArea(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: 10),
+          child: Column(
+            children: [
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 18),
+                child: Text(
+                  'Управление расписанием и группами',
+                  style: DarkTextTheme.h6,
+                ),
               ),
-            ),
-            // Column(
-            //   children: [
-            //     Row(
-            //       mainAxisAlignment: MainAxisAlignment.spaceBetween,
-            //       children: [
-            //         Row(
-            //           children: [
-            //             SvgPicture.asset(
-            //               'assets/icons/lessons.svg',
-            //               height: 16,
-            //               width: 16,
-            //             ),
-            //             SizedBox(width: 20),
-            //             Text("Пустые пары", style: DarkTextTheme.buttonL),
-            //           ],
-            //         ),
-            //         Padding(
-            //           padding: EdgeInsets.only(right: 8),
-            //           child: ValueListenableBuilder(
-            //             valueListenable: _switchValueNotifier,
-            //             builder: (context, hasError, child) => CupertinoSwitch(
-            //               activeColor: DarkThemeColors.primary,
-            //               value: _switchValueNotifier.value,
-            //               onChanged: (value) {
-            //                 _switchValueNotifier.value = value;
-            //               },
-            //             ),
-            //           ),
-            //         ),
-            //       ],
-            //     ),
-            //     SizedBox(height: 20),
-            //     Opacity(
-            //       opacity: 0.05,
-            //       child: Container(
-            //         width: double.infinity,
-            //         height: 1,
-            //         color: Colors.white,
-            //       ),
-            //     ),
-            //   ],
-            // ),
-            BlocBuilder<ScheduleBloc, ScheduleState>(
-                buildWhen: (prevState, currentState) {
-              if (currentState is ScheduleLoaded &&
-                  prevState.runtimeType != ScheduleLoaded)
-                return true;
-              else if (currentState is ScheduleLoaded &&
-                  prevState is ScheduleLoaded) {
-                if (prevState.activeGroup != currentState.activeGroup ||
-                    prevState.downloadedScheduleGroups !=
-                        currentState.downloadedScheduleGroups) return true;
-              }
-              return false;
-            }, builder: (context, state) {
-              if (state is ScheduleLoaded) {
-                return Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
+              Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      InkWell(
-                        child: Column(
-                          children: [
-                            Padding(
-                              padding: EdgeInsets.symmetric(
-                                  vertical: 20, horizontal: 8),
-                              child: Row(
-                                children: [
-                                  SvgPicture.asset(
-                                    'assets/icons/add_group.svg',
-                                    height: 16,
-                                    width: 16,
-                                  ),
-                                  SizedBox(width: 20),
-                                  Text("Добавить группу",
-                                      style: DarkTextTheme.buttonL),
-                                ],
-                              ),
-                            ),
-                            Opacity(
-                              opacity: 0.05,
-                              child: Container(
-                                width: double.infinity,
-                                height: 1,
-                                color: Colors.white,
-                              ),
-                            ),
-                          ],
-                        ),
-                        onTap: () {
-                          if (!_modalShown)
-                            showModalBottomSheet(
-                              context: context,
-                              isScrollControlled: true,
-                              backgroundColor: Colors.transparent,
-                              builder: (context) => ScheduleSettingsModal(
-                                  groups: state.groups, isFirstRun: false),
-                            ).whenComplete(() {
-                              this._modalShown = false;
-                            });
-                          this._modalShown = true;
-                        },
+                      Row(
+                        children: [
+                          SvgPicture.asset(
+                            'assets/icons/lessons.svg',
+                            height: 16,
+                            width: 16,
+                          ),
+                          SizedBox(width: 20),
+                          Text("Пустые пары", style: DarkTextTheme.buttonL),
+                        ],
                       ),
-                      SizedBox(height: 20),
-                      Text(
-                        "Группы".toUpperCase(),
-                        style: DarkTextTheme.chip
-                            .copyWith(color: DarkThemeColors.deactiveDarker),
-                        textAlign: TextAlign.left,
-                      ),
-                      SizedBox(height: 10),
-                      _buildGroupButton(
-                          state.activeGroup, state.activeGroup, true),
-                      Expanded(
-                        child: ListView.builder(
-                          itemCount: state.downloadedScheduleGroups.length,
-                          itemBuilder: (context, index) {
-                            if (state.downloadedScheduleGroups[index] !=
-                                state.activeGroup)
-                              return _buildGroupButton(
-                                  state.downloadedScheduleGroups[index],
-                                  state.activeGroup,
-                                  false,
-                                  state.schedule);
-                            return Container();
-                          },
+                      Padding(
+                        padding: EdgeInsets.only(right: 8),
+                        child: ValueListenableBuilder(
+                          valueListenable: _switchValueNotifier,
+                          builder: (context, hasError, child) =>
+                              CupertinoSwitch(
+                            activeColor: DarkThemeColors.primary,
+                            value: _switchValueNotifier.value,
+                            onChanged: (value) {
+                              _switchValueNotifier.value = value;
+                            },
+                          ),
                         ),
                       ),
                     ],
                   ),
-                );
-              }
-              return Container();
-            }),
+                  SizedBox(height: 20),
+                  Opacity(
+                    opacity: 0.05,
+                    child: Container(
+                      width: double.infinity,
+                      height: 1,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
+              ),
+              BlocBuilder<ScheduleBloc, ScheduleState>(
+                  buildWhen: (prevState, currentState) {
+                if (currentState is ScheduleLoaded &&
+                    prevState is ScheduleLoaded) {
+                  if (prevState.activeGroup != currentState.activeGroup ||
+                      prevState.downloadedScheduleGroups !=
+                          currentState.downloadedScheduleGroups) return true;
+                }
+                if (currentState is ScheduleLoaded &&
+                    prevState.runtimeType != ScheduleLoaded) return true;
+                return false;
+              }, builder: (context, state) {
+                if (state is ScheduleLoaded) {
+                  return Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        InkWell(
+                          child: Column(
+                            children: [
+                              Padding(
+                                padding: EdgeInsets.symmetric(vertical: 20),
+                                child: Row(
+                                  children: [
+                                    SvgPicture.asset(
+                                      'assets/icons/add_group.svg',
+                                      height: 16,
+                                      width: 16,
+                                    ),
+                                    SizedBox(width: 20),
+                                    Text("Добавить группу",
+                                        style: DarkTextTheme.buttonL),
+                                  ],
+                                ),
+                              ),
+                              Opacity(
+                                opacity: 0.05,
+                                child: Container(
+                                  width: double.infinity,
+                                  height: 1,
+                                  color: Colors.white,
+                                ),
+                              ),
+                            ],
+                          ),
+                          onTap: () {
+                            if (!_modalShown)
+                              showModalBottomSheet(
+                                context: context,
+                                isScrollControlled: true,
+                                backgroundColor: Colors.transparent,
+                                builder: (context) => ScheduleSettingsModal(
+                                    groups: state.groups, isFirstRun: false),
+                              ).whenComplete(() {
+                                this._modalShown = false;
+                              });
+                            this._modalShown = true;
+                          },
+                        ),
+                        SizedBox(height: 20),
+                        Text(
+                          "Группы".toUpperCase(),
+                          style: DarkTextTheme.chip
+                              .copyWith(color: DarkThemeColors.deactiveDarker),
+                          textAlign: TextAlign.left,
+                        ),
+                        SizedBox(height: 10),
+                        _buildGroupButton(
+                            state.activeGroup, state.activeGroup, true),
+                        Expanded(
+                          child: ListView.builder(
+                            itemCount: state.downloadedScheduleGroups.length,
+                            itemBuilder: (context, index) {
+                              if (state.downloadedScheduleGroups[index] !=
+                                  state.activeGroup)
+                                return _buildGroupButton(
+                                    state.downloadedScheduleGroups[index],
+                                    state.activeGroup,
+                                    false,
+                                    state.schedule);
+                              return Container();
+                            },
+                          ),
+                        ),
+                      ],
+                    ),
+                  );
+                } else
+                  return Container();
+              }),
+            ],
+          ),
+        ),
+      ),
+      scaffold: Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          title: Text(
+            'Расписание',
+            style: DarkTextTheme.title,
+          ),
+          actions: [
+            IconButton(
+                icon: Icon(Icons.dehaze),
+                onPressed: () {
+                  _innerDrawerKey.currentState!.toggle();
+                }),
           ],
         ),
-        sliderMain: SafeArea(
+        body: SafeArea(
           child: BlocBuilder<ScheduleBloc, ScheduleState>(
             buildWhen: (prevState, currentState) {
               if (prevState is ScheduleLoaded && currentState is ScheduleLoaded)
                 return prevState.schedule != currentState.schedule
                     ? true
                     : false;
-              else
-                return prevState != currentState ? true : false;
+              return true;
             },
             builder: (context, state) {
               if (state is ScheduleInitial) {
@@ -344,81 +358,3 @@ class _ScheduleScreenState extends State<ScheduleScreen> {
     );
   }
 }
-
-// class ScheduleScreen extends StatelessWidget {
-//   static const String routeName = '/schedule';
-
-//   bool modalShown = false;
-
-//   GlobalKey<SliderMenuContainerState> _key =
-//       new GlobalKey<SliderMenuContainerState>();
-
-//   @override
-//   Widget build(BuildContext context) {
-//     // return Scaffold(
-//     //   appBar: AppBar(
-//     //     automaticallyImplyLeading: false,
-//     //     title: Text(
-//     //       'Расписание',
-//     //       style: DarkTextTheme.title,
-//     //     ),
-//     //   ),
-//     //   body: SafeArea(
-//     //     child: BlocBuilder<ScheduleBloc, ScheduleState>(
-//     //       builder: (context, state) {
-//     //         if (state is ScheduleInitial) {
-//     //           context.read<ScheduleBloc>().add(ScheduleOpenEvent());
-//     //           return Container();
-//     //         } else if (state is ScheduleActiveGroupEmpty) {
-//     //           WidgetsBinding.instance!.addPostFrameCallback(
-//     //             (_) {
-//     //               if (!modalShown)
-//     //                 showModalBottomSheet(
-//     //                   context: context,
-//     //                   isScrollControlled: true,
-//     //                   backgroundColor: Colors.transparent,
-//     //                   builder: (context) =>
-//     //                       ScheduleSettingsModal(groups: state.groups),
-//     //                 );
-//     //               this.modalShown = true;
-//     //             },
-//     //           );
-
-//     //           return Container();
-//     //         } else if (state is ScheduleLoading) {
-//     //           if (modalShown) {
-//     //             modalShown = false;
-//     //             Navigator.pop(context);
-//     //           }
-//     //           return Center(
-//     //             child: CircularProgressIndicator(
-//     //               backgroundColor: DarkThemeColors.primary,
-//     //               strokeWidth: 5,
-//     //             ),
-//     //           );
-//     //         } else if (state is ScheduleLoaded) {
-//     //           return SchedulePageView();
-//     //         } else if (state is ScheduleLoadError) {
-//     //           return Column(
-//     //             children: [
-//     //               Text(
-//     //                 'Упс!',
-//     //                 style: DarkTextTheme.h3,
-//     //               ),
-//     //               SizedBox(
-//     //                 height: 24,
-//     //               ),
-//     //               Text(
-//     //                 state.errorMessage,
-//     //                 style: DarkTextTheme.bodyBold,
-//     //               )
-//     //             ],
-//     //           );
-//     //         } else
-//     //           return Container();
-//     //       },
-//     //     ),
-//     //   ),
-//     // );
-//   }
-// }
