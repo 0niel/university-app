05719836d4cb03236e901d114eaeab5bf8b13f8c
TinyClayMan <70881505+TinyClayMan@users.noreply.github.com>
2021-12-28 18:16:27 +0300

    NewNode implemented
    Building a peer-to-peer and a device-to-device network for maintaining access even when WiFi and cellular network fail you

--- android/app/src/main/kotlin/ninja/mirea/mireaapp/MainActivity.kt
+++ android/app/src/main/kotlin/ninja/mirea/mireaapp/MainActivity.kt
@@ -1,6 +1,12 @@
 package ninja.mirea.mireaapp
 
+import android.os.Bundle;
 import io.flutter.embedding.android.FlutterActivity
+import com.clostra.newnode.NewNode;
 
 class MainActivity: FlutterActivity() {
+    override protected fun onCreate(savedInstanceState: Bundle) {
+        super.onCreate(savedInstanceState)
+        NewNode.init()
+    }
 }
