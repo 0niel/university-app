d7d34809b734a0946028ec2d16cf4b49ce2c2784
Oniel <51058739+0niel@users.noreply.github.com>
2022-01-31 20:55:35 +0300

    FIX: added onLinkTap action to the profile announces page

--- lib/presentation/pages/profile/profile_announces_page.dart
+++ lib/presentation/pages/profile/profile_announces_page.dart
@@ -4,6 +4,7 @@ import 'package:flutter_html/flutter_html.dart';
 import 'package:rtu_mirea_app/presentation/bloc/announces_bloc/announces_bloc.dart';
 import 'package:rtu_mirea_app/presentation/colors.dart';
 import 'package:rtu_mirea_app/presentation/theme.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class ProfileAnnouncesPage extends StatelessWidget {
   const ProfileAnnouncesPage({Key? key}) : super(key: key);
@@ -48,6 +49,11 @@ class ProfileAnnouncesPage extends StatelessWidget {
                         style: {
                           "body": Style(
                               fontStyle: DarkTextTheme.bodyRegular.fontStyle),
+                        },
+                        onLinkTap: (String? url, context, attributes, element) {
+                          if (url != null) {
+                            launch(url);
+                          }
                         },
                       ),
                       const SizedBox(height: 13),
