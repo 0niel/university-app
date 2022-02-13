6c4e66400fa391402e069bfc67f6e772972390a3
Oniel <51058739+0niel@users.noreply.github.com>
2022-02-13 14:20:00 +0300

    UX: add stories hiding with scroll

--- lib/presentation/pages/news/news_page.dart
+++ lib/presentation/pages/news/news_page.dart
@@ -110,123 +110,136 @@ class _NewsPageState extends State<NewsPage> {
 
   @override
   Widget build(BuildContext context) {
-    _setupScrollController(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text("Новости"),
         backgroundColor: DarkThemeColors.background01,
       ),
       backgroundColor: DarkThemeColors.background01,
-      body: RefreshIndicator(
-        onRefresh: () async {
-          context.read<NewsBloc>().add(NewsLoadEvent(
-              refresh: true, isImportant: _tabValueNotifier.value == 1));
-          context.read<StoriesBloc>().add(LoadStories());
-        },
-        child: Column(children: [
-          BlocBuilder<StoriesBloc, StoriesState>(builder: (context, state) {
-            if (state is StoriesInitial) {
-              context.read<StoriesBloc>().add(LoadStories());
-            } else if (state is StoriesLoading) {
-              return Container();
-            } else if (state is StoriesLoaded) {
-              for (final story in state.stories) {
-                if (DateTime.now().compareTo(story.stopShowDate) == -1) {
-                  return SizedBox(
-                    height: 120,
-                    child: ListView.separated(
-                        padding: const EdgeInsets.symmetric(horizontal: 20),
-                        scrollDirection: Axis.horizontal,
-                        itemBuilder: (_, int i) {
-                          if (DateTime.now()
-                                  .compareTo(state.stories[i].stopShowDate) ==
-                              -1) {
-                            return StoryWidget(
-                              stories: state.stories,
-                              storyIndex: i,
-                            );
-                          }
-                          return Container();
-                        },
-                        separatorBuilder: (_, int i) =>
-                            const SizedBox(width: 10),
-                        itemCount: state.stories.length),
-                  );
+      body: NestedScrollView(
+        controller: _scrollController,
+        headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+          return [
+            SliverAppBar(
+              expandedHeight: 110,
+              actionsIconTheme: const IconThemeData(opacity: 0.0),
+              flexibleSpace: BlocBuilder<StoriesBloc, StoriesState>(
+                  builder: (context, state) {
+                if (state is StoriesInitial) {
+                  context.read<StoriesBloc>().add(LoadStories());
+                } else if (state is StoriesLoading) {
+                  return Container();
+                } else if (state is StoriesLoaded) {
+                  for (final story in state.stories) {
+                    if (DateTime.now().compareTo(story.stopShowDate) == -1) {
+                      return SizedBox(
+                        height: 120,
+                        child: ListView.separated(
+                            padding: const EdgeInsets.symmetric(horizontal: 20),
+                            scrollDirection: Axis.horizontal,
+                            itemBuilder: (_, int i) {
+                              if (DateTime.now().compareTo(
+                                      state.stories[i].stopShowDate) ==
+                                  -1) {
+                                return StoryWidget(
+                                  stories: state.stories,
+                                  storyIndex: i,
+                                );
+                              }
+                              return Container();
+                            },
+                            separatorBuilder: (_, int i) =>
+                                const SizedBox(width: 10),
+                            itemCount: state.stories.length),
+                      );
+                    }
+                  }
                 }
-              }
-            }
-            return Container();
-          }),
-          const SizedBox(height: 16),
-          _buildTabButtons(context),
-          Expanded(
-            child: BlocBuilder<NewsBloc, NewsState>(
-              builder: (context, state) {
-                List<NewsItem> news = [];
-                bool isLoading = false;
-
-                if (state is NewsInitial) {
-                  context.read<NewsBloc>().add(
-                      NewsLoadEvent(isImportant: _tabValueNotifier.value == 1));
-                } else if (state is NewsLoaded) {
-                  news = state.news;
-                } else if (state is NewsLoading && state.isFirstFetch) {
-                  return const Center(
-                    child: CircularProgressIndicator(),
-                  );
-                } else if (state is NewsLoading) {
-                  news = state.oldNews;
-                  isLoading = true;
-                } else if (state is NewsLoadError) {
-                  return SingleChildScrollView(
-                    physics: const AlwaysScrollableScrollPhysics(),
-                    child: Center(
-                      child: Text(
-                        'Произошла ошибка при загрузке новостей.',
-                        textAlign: TextAlign.center,
-                        style: DarkTextTheme.title,
-                      ),
-                    ),
-                  );
-                }
-                return Column(
-                  children: [
-                    const SizedBox(height: 12),
-                    Expanded(
-                      child: ListView.builder(
-                        controller: _scrollController,
-                        itemBuilder: (context, index) {
-                          if (index < news.length) {
-                            return NewsItemWidget(
-                                newsItem: news[index],
-                                onClickNewsTag: (tag) => _filterNewsByTag(
-                                    context.read<NewsBloc>(), tag));
-                          } else {
-                            Timer(const Duration(milliseconds: 30), () {
-                              _scrollController.jumpTo(
-                                  _scrollController.position.maxScrollExtent);
-                            });
-                            return const Center(
-                                child: CircularProgressIndicator());
-                          }
-                        },
-                        itemCount: news.length + (isLoading ? 1 : 0),
-                      ),
-                    ),
-                  ],
-                );
-              },
+                return Container();
+              }),
             ),
-          ),
-        ]),
+          ];
+        },
+        body: Builder(
+          builder: (BuildContext context) {
+            final innerScrollController = PrimaryScrollController.of(context);
+            _setupScrollController(innerScrollController!);
+
+            return RefreshIndicator(
+              onRefresh: () async {
+                context.read<NewsBloc>().add(NewsLoadEvent(
+                    refresh: true, isImportant: _tabValueNotifier.value == 1));
+                context.read<StoriesBloc>().add(LoadStories());
+              },
+              child: Column(children: [
+                const SizedBox(height: 16),
+                _buildTabButtons(context),
+                BlocBuilder<NewsBloc, NewsState>(
+                  builder: (context, state) {
+                    List<NewsItem> news = [];
+                    bool isLoading = false;
+
+                    if (state is NewsInitial) {
+                      context.read<NewsBloc>().add(NewsLoadEvent(
+                          isImportant: _tabValueNotifier.value == 1));
+                    } else if (state is NewsLoaded) {
+                      news = state.news;
+                    } else if (state is NewsLoading && state.isFirstFetch) {
+                      return const Center(
+                        child: CircularProgressIndicator(),
+                      );
+                    } else if (state is NewsLoading) {
+                      news = state.oldNews;
+                      isLoading = true;
+                    } else if (state is NewsLoadError) {
+                      return SingleChildScrollView(
+                        physics: const AlwaysScrollableScrollPhysics(),
+                        child: Center(
+                          child: Text(
+                            'Произошла ошибка при загрузке новостей.',
+                            textAlign: TextAlign.center,
+                            style: DarkTextTheme.title,
+                          ),
+                        ),
+                      );
+                    }
+                    return Expanded(
+                      child: Column(
+                        children: [
+                          const SizedBox(height: 12),
+                          Expanded(
+                            child: ListView.builder(
+                              itemCount: news.length + (isLoading ? 1 : 0),
+                              itemBuilder: (context, index) {
+                                if (index < news.length) {
+                                  return NewsItemWidget(
+                                      newsItem: news[index],
+                                      onClickNewsTag: (tag) => _filterNewsByTag(
+                                          context.read<NewsBloc>(), tag));
+                                } else {
+                                  return const Center(
+                                      child: CircularProgressIndicator());
+                                }
+                              },
+                            ),
+                          ),
+                        ],
+                      ),
+                    );
+                  },
+                ),
+              ]),
+            );
+          },
+        ),
       ),
     );
   }
 
-  void _setupScrollController(BuildContext context) {
-    _scrollController.addListener(() {
-      if (_scrollController.position.atEdge) {
-        if (_scrollController.position.pixels != 0) {
+  void _setupScrollController(ScrollController controller) {
+    controller.addListener(() {
+      if (controller.position.atEdge) {
+        if (controller.position.pixels != 0) {
           context
               .read<NewsBloc>()
               .add(NewsLoadEvent(isImportant: _tabValueNotifier.value == 1));
