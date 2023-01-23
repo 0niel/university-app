0bc9c59aba4beec637da5b686712d697f445fbbd
Sergey Dmitriev <51058739+0niel@users.noreply.github.com>
2023-01-23 23:48:33 +0300

    ui: Add Bottom Navigation Bar Background (#259)
    * ui: Add Bottom Navigation Bar Background
    
    У нижней навигационной панели не было цвета, что хорошо заметно на страницах, у которых фон отличается от стандартного. Например, на странице карты навигационная панель просвечивалась.
    
    * dev: Remove unused imports

--- lib/presentation/pages/home_page.dart
+++ lib/presentation/pages/home_page.dart
@@ -2,10 +2,8 @@ import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:rtu_mirea_app/presentation/bloc/app_cubit/app_cubit.dart';
-import 'package:rtu_mirea_app/presentation/bloc/update_info_bloc/update_info_bloc.dart';
 import 'package:rtu_mirea_app/presentation/colors.dart';
 import 'package:rtu_mirea_app/presentation/core/routes/routes.gr.dart';
-import 'package:rtu_mirea_app/presentation/widgets/update_info_modal.dart';
 import 'package:salomon_bottom_bar/salomon_bottom_bar.dart';
 
 class HomePage extends StatelessWidget {
@@ -57,35 +55,38 @@ class AppBottomNavigationBar extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SalomonBottomBar(
-      margin: const EdgeInsets.symmetric(
-        horizontal: 10,
-        vertical: 10,
+    return Container(
+      color: DarkThemeColors.background01,
+      child: SalomonBottomBar(
+        margin: const EdgeInsets.symmetric(
+          horizontal: 10,
+          vertical: 10,
+        ),
+        currentIndex: index,
+        onTap: onClick,
+        items: [
+          SalomonBottomBarItem(
+            icon: const Icon(Icons.library_books_rounded),
+            title: const Text("Новости"),
+            selectedColor: DarkThemeColors.primary,
+          ),
+          SalomonBottomBarItem(
+            icon: const Icon(Icons.calendar_today_rounded),
+            title: const Text("Расписание"),
+            selectedColor: DarkThemeColors.primary,
+          ),
+          SalomonBottomBarItem(
+            icon: const Icon(Icons.map_rounded),
+            title: const Text("Карта"),
+            selectedColor: DarkThemeColors.primary,
+          ),
+          SalomonBottomBarItem(
+            icon: const Icon(Icons.person),
+            title: const Text("Профиль"),
+            selectedColor: DarkThemeColors.primary,
+          ),
+        ],
       ),
-      currentIndex: index,
-      onTap: onClick,
-      items: [
-        SalomonBottomBarItem(
-          icon: const Icon(Icons.library_books_rounded),
-          title: const Text("Новости"),
-          selectedColor: DarkThemeColors.primary,
-        ),
-        SalomonBottomBarItem(
-          icon: const Icon(Icons.calendar_today_rounded),
-          title: const Text("Расписание"),
-          selectedColor: DarkThemeColors.primary,
-        ),
-        SalomonBottomBarItem(
-          icon: const Icon(Icons.map_rounded),
-          title: const Text("Карта"),
-          selectedColor: DarkThemeColors.primary,
-        ),
-        SalomonBottomBarItem(
-          icon: const Icon(Icons.person),
-          title: const Text("Профиль"),
-          selectedColor: DarkThemeColors.primary,
-        ),
-      ],
     );
   }
 }
