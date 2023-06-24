3b95a1d76aa1c0aab2b859c6c746f0f982ff7745
Sergey Dmitriev <51058739+0niel@users.noreply.github.com>
2023-06-24 21:41:07 +0300

    refactor: Change oauth urls and add env consts (#326)

---
+++ lib/common/constants.dart
@@ -0,0 +1,4 @@
+const sentryDsn = String.fromEnvironment('SENTRY_DSN');
+
+const lkClientId = String.fromEnvironment('LK_CLIENT_ID');
+const lkClientSecret = String.fromEnvironment('LK_CLIENT_SECRET');
