330ae42b8b00eaaacdf0d1c6d43cfc59b962746c
Sergey Dmitriev <51058739+0niel@users.noreply.github.com>
2024-01-17 22:55:21 +0300

    feat(api): Add Campuses cords

--- api/packages/rtu_mirea_schedule_api_client/lib/src/campuses.dart
+++ api/packages/rtu_mirea_schedule_api_client/lib/src/campuses.dart
@@ -1,10 +1,35 @@
 import 'package:schedule/schedule.dart';
 
 /// List of all RTU MIREA campuses.
-const List<Campus> campuses = [
-  Campus(name: 'ул. Малая Пироговская, д.1', shortName: 'МП-1'),
-  Campus(name: 'Проспект Вернадского, д.78', shortName: 'В-78'),
-  Campus(name: 'Проспект Вернадского, д.86', shortName: 'В-86'),
-  Campus(name: 'ул. Стромынка, 20', shortName: 'С-20'),
-  Campus(name: '5-я ул. Соколиной горы, д.22', shortName: 'СГ-22'),
+List<Campus> campuses = [
+  const Campus(
+    name: 'ул. Малая Пироговская, д.1',
+    shortName: 'МП-1',
+    latitude: 55.731582,
+    longitude: 37.574840,
+  ),
+  const Campus(
+    name: 'Проспект Вернадского, д.78',
+    shortName: 'В-78',
+    latitude: 55.669957,
+    longitude: 37.480225,
+  ),
+  const Campus(
+    name: 'Проспект Вернадского, д.86',
+    shortName: 'В-86',
+    latitude: 55.661445,
+    longitude: 37.477049,
+  ),
+  const Campus(
+    name: 'ул. Стромынка, 20',
+    shortName: 'С-20',
+    latitude: 55.794259,
+    longitude: 37.701448,
+  ),
+  const Campus(
+    name: '5-я ул. Соколиной горы, д.22',
+    shortName: 'СГ-22',
+    latitude: 55.764911,
+    longitude: 37.741962,
+  ),
 ];
