c6c42cf1bb7b4e61ad1dcf388a771211ad6a8c63
Sergey Dmitriev <51058739+0niel@users.noreply.github.com>
2024-03-03 15:21:22 +0300

    feat: Create news repository

---
+++ packages/news_repository/lib/news_repository.dart
@@ -0,0 +1,3 @@
+export 'package:university_app_server_api/client.dart' show NewsFeedResponse, NewsItemResponse;
+
+export 'src/news_repository.dart';
---
+++ packages/news_repository/lib/src/news_repository.dart
@@ -0,0 +1,56 @@
+import 'package:equatable/equatable.dart';
+import 'package:university_app_server_api/client.dart';
+
+/// {@template news_failure}
+/// Base failure class for the news feature.
+/// {@endtemplate}
+abstract class NewsFailure with EquatableMixin implements Exception {
+  /// {@macro news_failure}
+  const NewsFailure(this.error);
+
+  /// The error which was caught.
+  final Object error;
+
+  @override
+  List<Object?> get props => [error];
+}
+
+class GetNewsFailure extends NewsFailure {
+  const GetNewsFailure(super.error);
+}
+
+class GetAdsFailure extends NewsFailure {
+  const GetAdsFailure(super.error);
+}
+
+/// {@template community_repository}
+/// A repository that manages news data.
+/// {@endtemplate}
+class NewsRepository {
+  /// {@macro community_repository}
+  const NewsRepository({
+    required ApiClient apiClient,
+  }) : _apiClient = apiClient;
+
+  final ApiClient _apiClient;
+
+  Future<NewsFeedResponse> getSponsors({
+    int? limit,
+    int? offset,
+    String? category,
+  }) async {
+    try {
+      return await _apiClient.getNews(limit: limit, offset: offset, category: category);
+    } catch (error, stackTrace) {
+      Error.throwWithStackTrace(GetNewsFailure(error), stackTrace);
+    }
+  }
+
+  Future<NewsFeedResponse> getAds({int? limit, int? offset}) async {
+    try {
+      return await _apiClient.getAds(limit: limit, offset: offset);
+    } catch (error, stackTrace) {
+      Error.throwWithStackTrace(GetAdsFailure(error), stackTrace);
+    }
+  }
+}
