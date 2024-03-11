1bcb921f40b6e1a3a5e39ceace23ba50d581c6b9
Sergey Dmitriev <51058739+0niel@users.noreply.github.com>
2024-03-11 15:44:31 +0300

    fix: Make doggy image overflowed

--- lib/schedule/widgets/holiday_page.dart
+++ lib/schedule/widgets/holiday_page.dart
@@ -8,23 +8,28 @@ class HolidayPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      mainAxisAlignment: MainAxisAlignment.center,
-      children: [
-        const Image(
-          image: AssetImage('assets/images/Saly-18.png'),
-          height: 225.0,
-        ),
-        Text(title, style: AppTextStyle.title),
-        const SizedBox(
-          height: 8,
-        ),
-        Text(
-          "Пар в этот день нет!",
-          style: AppTextStyle.bodyL,
-        ),
-      ],
+    return OverflowBox(
+      alignment: Alignment.topLeft,
+      maxWidth: double.infinity,
+      maxHeight: double.infinity,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Image(
+            image: AssetImage('assets/images/Saly-18.png'),
+            height: 225.0,
+          ),
+          Text(title, style: AppTextStyle.title),
+          const SizedBox(
+            height: 8,
+          ),
+          Text(
+            "Пар в этот день нет!",
+            style: AppTextStyle.bodyL,
+          ),
+        ],
+      ),
     );
   }
 }
