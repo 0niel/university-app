b0d0c2f417ac7a84859e7e40967df738d45a9282
Sergey Dmitriev <51058739+0niel@users.noreply.github.com>
2024-09-02 22:48:13 +0300

    chore: Remove unused files and directories

--- aurora/main.cpp
+++
@@ -1,9 +0,0 @@
-#include <flutter/application.h>
-#include "generated_plugin_registrant.h"
-
-int main(int argc, char *argv[]) {
-    Application::Initialize(argc, argv);
-    RegisterPlugins();
-    Application::Launch();
-    return 0;
-}
