cbbb239c51011eb43a248ff238317556468e3c22
Sergey Dmitriev <51058739+0niel@users.noreply.github.com>
2024-09-03 14:09:53 +0300

    ui: Update search page layout and add search title

--- lib/search/view/search_page.dart
+++ lib/search/view/search_page.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
 import 'package:rtu_mirea_app/presentation/theme.dart';
+import 'package:rtu_mirea_app/presentation/typography.dart';
 import 'package:rtu_mirea_app/schedule/bloc/schedule_bloc.dart';
 import 'package:rtu_mirea_app/search/widgets/search_history_item.dart';
 import 'package:schedule_repository/schedule_repository.dart';
@@ -91,115 +93,136 @@ class _SearchViewState extends State<SearchView> {
                     ),
                   ),
                   const SearchModeSelect(),
-                  Padding(
-                    padding: const EdgeInsets.symmetric(horizontal: 16),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        ..._controller.text.isNotEmpty || state.searchHisoty.isEmpty
-                            ? []
-                            : [
-                                const Row(
-                                  crossAxisAlignment: CrossAxisAlignment.center,
-                                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    SearchHeadlineText(
-                                      headerText: "История",
+                  if (_controller.text.isNotEmpty)
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 16),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          ..._controller.text.isNotEmpty || state.searchHisoty.isEmpty
+                              ? []
+                              : [
+                                  const Row(
+                                    crossAxisAlignment: CrossAxisAlignment.center,
+                                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      SearchHeadlineText(
+                                        headerText: "История",
+                                      ),
+                                      ClearSearchHistoryButton(),
+                                    ],
+                                  ),
+                                  ...state.searchHisoty.map(
+                                    (e) => SearchHistoryItem(
+                                      query: e,
+                                      onPressed: (value) {
+                                        _controller.text = value;
+                                      },
+                                      onClear: () {
+                                        context.read<SearchBloc>().add(RemoveQueryFromSearchHistory(query: e));
+                                      },
                                     ),
-                                    ClearSearchHistoryButton(),
-                                  ],
-                                ),
-                                ...state.searchHisoty.map(
-                                  (e) => SearchHistoryItem(
-                                    query: e,
-                                    onPressed: (value) {
-                                      _controller.text = value;
-                                    },
-                                    onClear: () {
-                                      context.read<SearchBloc>().add(RemoveQueryFromSearchHistory(query: e));
-                                    },
                                   ),
-                                ),
-                              ],
-                        ...state.groups.results.isEmpty
-                            ? []
-                            : [
-                                const SearchHeadlineText(
-                                  headerText: "Группы",
-                                ),
-                                ...state.groups.results.map<Widget>(
-                                  (group) => SearchResultItem(
-                                    name: group.name,
-                                    type: ItemType.group,
-                                    onPressed: () => {
-                                      context.read<ScheduleBloc>().add(ScheduleRequested(group: group)),
-                                      context.go(
-                                        '/schedule',
-                                      ),
-                                      context.read<SearchBloc>().add(
-                                            AddQueryToSearchHistory(query: group.name),
-                                          ),
-                                    },
+                                ],
+                          ...state.groups.results.isEmpty
+                              ? []
+                              : [
+                                  const SearchHeadlineText(
+                                    headerText: "Группы",
                                   ),
-                                ),
-                              ],
-                        ...state.teachers.results.isEmpty
-                            ? []
-                            : [
-                                const SearchHeadlineText(
-                                  headerText: "Преподаватели",
-                                ),
-                                ...state.teachers.results.map<Widget>(
-                                  (teacher) => SearchResultItem(
-                                    name: teacher.name,
-                                    type: ItemType.teacher,
-                                    onPressed: () => {
-                                      context.read<ScheduleBloc>().add(TeacherScheduleRequested(teacher: teacher)),
-                                      context.go(
-                                        '/schedule',
-                                      ),
-                                      context.read<SearchBloc>().add(
-                                            AddQueryToSearchHistory(query: teacher.name),
-                                          ),
-                                    },
+                                  ...state.groups.results.map<Widget>(
+                                    (group) => SearchResultItem(
+                                      name: group.name,
+                                      type: ItemType.group,
+                                      onPressed: () => {
+                                        context.read<ScheduleBloc>().add(ScheduleRequested(group: group)),
+                                        context.go(
+                                          '/schedule',
+                                        ),
+                                        context.read<SearchBloc>().add(
+                                              AddQueryToSearchHistory(query: group.name),
+                                            ),
+                                      },
+                                    ),
                                   ),
-                                ),
-                              ],
-                        ...state.classrooms.results.isEmpty
-                            ? []
-                            : [
-                                const SearchHeadlineText(
-                                  headerText: "Аудитории",
-                                ),
-                                ...state.classrooms.results.map<Widget>(
-                                  (classroom) => SearchResultItem(
-                                    name:
-                                        '${classroom.name}${classroom.campus != null ? ' (${classroom.campus?.shortName ?? classroom.campus?.name})' : ''}',
-                                    type: ItemType.classroom,
-                                    onPressed: () => {
-                                      context
-                                          .read<ScheduleBloc>()
-                                          .add(ClassroomScheduleRequested(classroom: classroom)),
-                                      context.go(
-                                        '/schedule',
-                                      ),
-                                      context.read<SearchBloc>().add(
-                                            AddQueryToSearchHistory(query: classroom.name),
-                                          ),
-                                    },
+                                ],
+                          ...state.teachers.results.isEmpty
+                              ? []
+                              : [
+                                  const SearchHeadlineText(
+                                    headerText: "Преподаватели",
                                   ),
-                                ),
-                              ],
-                        if (state.status == SearchStatus.loading)
-                          const Center(
-                            child: Padding(
-                              padding: EdgeInsets.all(8.0),
-                              child: CircularProgressIndicator(),
+                                  ...state.teachers.results.map<Widget>(
+                                    (teacher) => SearchResultItem(
+                                      name: teacher.name,
+                                      type: ItemType.teacher,
+                                      onPressed: () => {
+                                        context.read<ScheduleBloc>().add(TeacherScheduleRequested(teacher: teacher)),
+                                        context.go(
+                                          '/schedule',
+                                        ),
+                                        context.read<SearchBloc>().add(
+                                              AddQueryToSearchHistory(query: teacher.name),
+                                            ),
+                                      },
+                                    ),
+                                  ),
+                                ],
+                          ...state.classrooms.results.isEmpty
+                              ? []
+                              : [
+                                  const SearchHeadlineText(
+                                    headerText: "Аудитории",
+                                  ),
+                                  ...state.classrooms.results.map<Widget>(
+                                    (classroom) => SearchResultItem(
+                                      name:
+                                          '${classroom.name}${classroom.campus != null ? ' (${classroom.campus?.shortName ?? classroom.campus?.name})' : ''}',
+                                      type: ItemType.classroom,
+                                      onPressed: () => {
+                                        context
+                                            .read<ScheduleBloc>()
+                                            .add(ClassroomScheduleRequested(classroom: classroom)),
+                                        context.go(
+                                          '/schedule',
+                                        ),
+                                        context.read<SearchBloc>().add(
+                                              AddQueryToSearchHistory(query: classroom.name),
+                                            ),
+                                      },
+                                    ),
+                                  ),
+                                ],
+                          if (state.status == SearchStatus.loading)
+                            const Center(
+                              child: Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: CircularProgressIndicator(),
+                              ),
                             ),
-                          ),
-                      ],
-                    ),
-                  ),
+                        ],
+                      ),
+                    )
+                  else
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height - 200,
+                      child: Center(
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            const Icon(
+                              Icons.search,
+                              size: 64,
+                            ),
+                            const SizedBox(height: 16),
+                            Padding(
+                              padding: const EdgeInsets.all(16.0),
+                              child: Text("Введите запрос для поиска", style: AppTextStyle.body),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ).animate().fadeIn(duration: 500.ms),
                 ],
               ),
             )
