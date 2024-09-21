1344ad4cdd54bb054f79a4fa6b934e1671617ffb
Sergey Dmitriev <51058739+0niel@users.noreply.github.com>
2024-09-21 19:04:47 +0300

    refactor: Update scroll physics in schedule page

--- lib/schedule/view/schedule_page.dart
+++ lib/schedule/view/schedule_page.dart
@@ -1,4 +1,5 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -416,7 +417,7 @@ class _SchedulePageState extends State<SchedulePage> {
 
         return ListView.builder(
           shrinkWrap: true,
-          physics: const NeverScrollableScrollPhysics(),
+          physics: Platform.isIOS ? const BouncingScrollPhysics() : const ClampingScrollPhysics(),
           itemCount: SchedulePage.maxLessonsPerDay,
           itemBuilder: (context, lessonIndex) {
             final lessons = lessonsByTime[lessonIndex + 1] ?? [];
