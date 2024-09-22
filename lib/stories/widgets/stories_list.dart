07b1a645e70e71686f077a2234902a5a8e497474
Sergey Dmitriev <51058739+0niel@users.noreply.github.com>
2024-09-23 01:37:17 +0300

    ui: Update story group styling

--- lib/stories/widgets/stories_list.dart
+++ lib/stories/widgets/stories_list.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:rtu_mirea_app/env/env.dart';
 import 'package:rtu_mirea_app/presentation/theme.dart';
 import 'package:storyly_flutter/storyly_flutter.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 
 class StoriesList extends StatefulWidget {
   const StoriesList({
@@ -24,7 +25,9 @@ class _StoriesListState extends State<StoriesList> {
 
   @override
   Widget build(BuildContext context) {
-    final params = StorylyParam()
+    final devicePixelRatio = MediaQuery.of(context).devicePixelRatio;
+
+    final iosParams = StorylyParam()
       ..storylyId = Env.storylyId
       ..storyGroupSize = "custom"
       ..storyGroupListHorizontalEdgePadding = 16
@@ -33,19 +36,41 @@ class _StoriesListState extends State<StoriesList> {
       ..storyGroupTextColorSeen = AppTheme.colorsOf(context).active
       ..storyGroupTextColorNotSeen = AppTheme.colorsOf(context).active
       ..storyGroupIconHeight = 70
+      ..storyGroupIconWidth = 70
       ..storyGroupTextLines = 2
       ..storyGroupIconCornerRadius = 50
-      ..storyGroupIconWidth = 70;
+      ..storylyLocale = 'ru-RU';
+
+    final androidParams = StorylyParam()
+      ..storylyId = Env.storylyId
+      ..storyGroupSize = "custom"
+      ..storyGroupListHorizontalEdgePadding = (16 * devicePixelRatio).round()
+      ..storyGroupListHorizontalPaddingBetweenItems = (8 * devicePixelRatio).round()
+      ..storyGroupTextIsVisible = true
+      ..storyGroupTextColorSeen = AppTheme.colorsOf(context).active
+      ..storyGroupTextColorNotSeen = AppTheme.colorsOf(context).active
+      ..storyGroupIconHeight = (70 * devicePixelRatio).round()
+      ..storyGroupIconWidth = (70 * devicePixelRatio).round()
+      ..storyGroupTextLines = 2
+      ..storyGroupIconCornerRadius = (50 * devicePixelRatio).round()
+      ..storylyLocale = 'ru-RU';
 
     return SizedBox(
-      height: 80,
+      height: 70,
       child: StorylyView(
         onStorylyViewCreated: onStorylyViewCreated,
-        androidParam: params,
-        iosParam: params,
+        androidParam: androidParams,
+        iosParam: iosParams,
         storylyLoaded: (storyGroups, dataSource) {
           final stories = storyGroups.map((st) => st.stories).expand((element) => element).toList();
           widget.onStoriesLoaded?.call(stories);
+        },
+        storylyActionClicked: (story) {
+          final url = story.actionUrl;
+
+          if (url != null) {
+            launchUrlString(url, mode: LaunchMode.externalApplication);
+          }
         },
       ),
     );
