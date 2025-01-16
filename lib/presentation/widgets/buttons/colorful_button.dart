e7cb7dcc8874e5488ca7875e4aa5ad5edaf369d8
Sergey Dmitriev <51058739+0niel@users.noreply.github.com>
2025-01-16 15:14:33 +0300

    feat: update ColorfulButton to use theme colors and improve styling

--- lib/presentation/widgets/buttons/colorful_button.dart
+++ lib/presentation/widgets/buttons/colorful_button.dart
@@ -1,5 +1,6 @@
 import 'package:enough_platform_widgets/enough_platform_widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:rtu_mirea_app/presentation/theme.dart';
 import 'package:rtu_mirea_app/presentation/typography.dart';
 
 class ColorfulButton extends StatelessWidget {
@@ -17,27 +18,26 @@ class ColorfulButton extends StatelessWidget {
         onPressed: () {
           onClick();
         },
-        // margin: const EdgeInsets.all(0),
-        // shape: RoundedRectangleBorder(
-        //   borderRadius: BorderRadius.circular(12.0),
-        // ),
         material: (_, __) => MaterialTextButtonData(
           style: TextButton.styleFrom(
             padding: EdgeInsets.zero,
             shape: RoundedRectangleBorder(
               borderRadius: BorderRadius.circular(12.0),
             ),
+            backgroundColor: backgroundColor,
           ),
         ),
         cupertino: (_, __) => CupertinoTextButtonData(
           padding: EdgeInsets.zero,
           borderRadius: BorderRadius.circular(12.0),
+          color: backgroundColor,
         ),
-        color: backgroundColor,
         child: Center(
           child: Text(
             text,
-            style: AppTextStyle.buttonL,
+            style: AppTextStyle.buttonL.copyWith(
+              color: AppTheme.colorsOf(context).active,
+            ),
           ),
         ),
       ),
