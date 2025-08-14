1ecf1c4532e49dae64782b6f1bc67ae7451592ad
Sergey Dmitriev <51058739+0niel@users.noreply.github.com>
2025-08-15 02:04:29 +0300

    feat(api): expand NewsDataSource interface with new methods for article retrieval, popular topics, and category management

--- api/lib/src/data/news/news_data_source.dart
+++ api/lib/src/data/news/news_data_source.dart
@@ -1,16 +1,73 @@
-import 'package:university_app_server_api/src/data/news/models/models.dart';
+import 'package:news_blocks/news_blocks.dart';
+import 'package:university_app_server_api/api.dart';
 
+/// {@template news_data_source}
+/// An interface for a news content data source.
+/// {@endtemplate}
 abstract class NewsDataSource {
-  Future<List<String>> getCategories();
+  /// {@macro news_data_source}
+  const NewsDataSource();
 
-  Future<List<Article>> getNews({
+  /// Returns a news [Article] for the provided article [id].
+  ///
+  /// In addition, the contents can be paginated by supplying
+  /// [limit] and [offset].
+  ///
+  /// * [limit] - The number of content blocks to return.
+  /// * [offset] - The (zero-based) offset of the first item
+  /// in the collection to return.
+  /// * [preview] - Whether to return a preview of the article.
+  ///
+  /// Returns `null` if there is no article with the provided [id].
+  Future<Article?> getArticle({
+    required String id,
     int limit = 20,
     int offset = 0,
-    String? category,
+    bool preview = false,
   });
 
-  Future<List<Article>> getAds({
+  /// Returns a list of current popular topics.
+  Future<List<String>> getPopularTopics();
+
+  /// Returns a list of current relevant topics
+  /// based on the provided [term].
+  Future<List<String>> getRelevantTopics({required String term});
+
+  /// Returns a list of current popular article blocks.
+  Future<List<NewsBlock>> getPopularArticles();
+
+  /// Returns a list of relevant article blocks
+  /// based on the provided [term].
+  Future<List<NewsBlock>> getRelevantArticles({required String term});
+
+  /// Returns [RelatedArticles] for the provided article [id].
+  ///
+  /// In addition, the contents can be paginated by supplying
+  /// [limit] and [offset].
+  ///
+  /// * [limit] - The number of content blocks to return.
+  /// * [offset] - The (zero-based) offset of the first item
+  /// in the collection to return.
+  Future<RelatedArticles> getRelatedArticles({
+    required String id,
     int limit = 20,
     int offset = 0,
   });
+
+  /// Returns a news [Feed] for the provided [categoryId].
+  ///
+  /// In addition, the feed can be paginated by supplying
+  /// [limit] and [offset].
+  ///
+  /// * [limit] - The number of results to return.
+  /// * [offset] - The (zero-based) offset of the first item
+  /// in the collection to return.
+  Future<Feed> getFeed({
+    required String categoryId,
+    int limit = 20,
+    int offset = 0,
+  });
+
+  /// Returns a list of all available news categories.
+  Future<List<Category>> getCategories();
 }
